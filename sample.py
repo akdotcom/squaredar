e10bd1f43aad1e09885bacb8719232a699a37b20
Akshay Patil <apatil@gmail.com>
2012-01-15 13:10:15 -0500

    Record friend distances on check-in
    Hacky first try and maintaing state. Need to stop doing the work while
    the push notification blocks.

--- sample.py
+++ sample.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
 
-import cgi
+import foursquare
 import logging
-import urllib2
+#import urllib2
+import time
+import urllib
 
-from django.utils import simplejson
+try: import simplejson as json
+except ImportError: import json
 
 from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+
             
 # prod
 config = {'server':'https://foursquare.com',
           'api_server':'https://api.foursquare.com',
-          'redirect_uri': 'http://4sq-push-api-sample.appspot.com/oauth',
-          'client_id': 'YOUR ID HERE',
-          'client_secret': 'YOUR SECRET'}
+          'redirect_uri': 'https://squaredar.appspot.com/oauth',
+#          'redirect_uri': 'http://localhost:8088/oauth',
+          'client_id': 'CLIENT_ID',
+          'client_secret': 'CLIENT_SECRET',
+          'push_secret': 'PUSH_SECRET'}
 
 class UserToken(db.Model):
   """Contains the user to foursquare_id + oauth token mapping."""
@@ -29,12 +36,50 @@ class Checkin(db.Model):
   fs_id = db.StringProperty()
   checkin_json = db.TextProperty()
 
+class FriendDistance(db.Model):
+  """Information about a friend's distance."""
+  fs_id = db.StringProperty()
+  friend_fs_id = db.StringProperty()
+  last_distance = db.IntegerProperty()
+  avg_distance = db.IntegerProperty()
+  num_points = db.IntegerProperty()
+  last_seen = db.IntegerProperty()
+  last_checkin = db.TextProperty()
+  def to_dict(self):
+    return dict([(p, unicode(getattr(self, p))) for p in self.properties()])
+
 def fetchJson(url):
   """Does a GET to the specified URL and returns a dict representing its reply."""
   logging.info('fetching url: ' + url)
-  result = urllib2.urlopen(url).read()
+  result = urllib.urlopen(url).read()
   logging.info('got back: ' + result)
-  return simplejson.loads(result)
+  return json.loads(result)
+
+def updateAvg(old_avg, old_num, point):
+  """Given an old average of a number of points, updates average with new point"""
+  new_total = (old_avg * old_num) + point
+  return new_total / (old_num + 1)
+
+def makeFriendDistance(friendDistance, avg_distance, last_distance,
+                       last_seen, last_checkin, id=None, friend_id=None,
+                       num_points=1):
+  if id:
+    friendDistance.fs_id = id
+  if friend_id:
+    friendDistance.friend_fs_id = friend_id
+  friendDistance.avg_distance = avg_distance
+  friendDistance.last_distance = last_distance
+  friendDistance.last_seen = last_seen;
+  friendDistance.last_checkin = last_checkin
+  friendDistance.num_points = num_points
+  return friendDistance
+
+
+def makeFoursquareClient(config, access_token=None):
+  return foursquare.Foursquare(client_id=config['client_id'],
+                               client_secret=config['client_secret'],
+                               redirect_uri=config['redirect_uri'],
+                               access_token=access_token)
 
 class OAuth(webapp.RequestHandler):
   """Handle the OAuth redirect back to the service."""
@@ -43,14 +88,15 @@ class OAuth(webapp.RequestHandler):
 
   def get(self):
     code = self.request.get('code')
-    args = dict(config)
-    args['code'] = code
-    url = ('%(server)s/oauth2/access_token?client_id=%(client_id)s&client_secret=%(client_secret)s&grant_type=authorization_code&redirect_uri=%(redirect_uri)s&code=%(code)s' % args)
-  
-    json = fetchJson(url)
+    logging.info('code ' + code)
+    
+    client = makeFoursquareClient(config)
+    access_token = client.oauth.get_token(code)
+    logging.info('token ' + access_token)
 
     token = UserToken()
-    token.token = json['access_token']
+#    token.token = json['access_token']
+    token.token = access_token
     token.user = users.get_current_user()
 
     self_response = fetchJson('%s/v2/users/self?oauth_token=%s' % (config['api_server'], token.token))
@@ -63,13 +109,71 @@ class OAuth(webapp.RequestHandler):
 class ReceiveCheckin(webapp.RequestHandler):
   """Received a pushed checkin and store it in the datastore."""
   def post(self):
-    json = simplejson.loads(self.request.body)
-    checkin_json = json['checkin']
-    user_json = json['user']
+    checkin_json = json.loads(self.request.get('checkin'))
+    user_json = checkin_json['user']
     checkin = Checkin()
     checkin.fs_id = user_json['id']
-    checkin.checkin_json = simplejson.dumps(checkin_json)
+    checkin.checkin_json = json.dumps(checkin_json)
     checkin.put()
+    fs_id = checkin.fs_id
+    lat = str(checkin_json['venue']['location']['lat'])
+    lng = str(checkin_json['venue']['location']['lng'])
+    latlng = lat+','+lng
+    user = UserToken.all().filter("fs_id = ", fs_id).get()
+    if user:
+      client = makeFoursquareClient(config, access_token=user.token)
+      recent_checkins = client.checkins.recent(params={'ll': latlng})
+      distance_map = {}
+      checkin_map = {}
+      for checkin in recent_checkins['recent']:
+        user_id = str(checkin['user']['id'])
+        distance_map[user_id] = checkin['distance']
+        checkin_map[user_id] = json.dumps(checkin)
+      logging.info(distance_map)
+      # Parallelize all of this with TaskQueues
+      friendDistances = FriendDistance.all().filter("fs_id = ", fs_id)
+
+      missed_friend_ids = distance_map.keys()
+      for friend in friendDistances:
+        if friend.friend_fs_id in distance_map:
+          missed_friend_ids.remove(friend.friend_fs_id)
+          distance = distance_map[friend.friend_fs_id]
+          avg_distance = friend.avg_distance
+          updated_avg_distance = updateAvg(avg_distance,
+                                          friend.num_points,
+                                          distance)
+          makeFriendDistance(friendDistance=friend,
+                             avg_distance=updated_avg_distance,
+                             last_distance=distance,
+                             last_seen=int(time.time()),
+                             last_checkin=checkin_map[friend.friend_fs_id],
+                             num_points=(friend.num_points + 1))
+
+#          friend.avg_distance = updateAvg(avg_distance,
+#                                          friend.num_points,
+#                                          distance)
+#          friend.last_distance = distance
+#          friend.last_seen = int(time.time());
+#          friend.last_checkin = checkin_map[friend.friend_fs_id]
+#          friend.num_points += 1
+          friend.put() # Bulk action? or perhaps parallelized
+          logging.info('Updated distance record for ' + friend.friend_fs_id)
+      for missed_friend_id in missed_friend_ids:
+        friend = FriendDistance()
+        makeFriendDistance(friendDistance=friend,
+                           id=fs_id,
+                           friend_id=missed_friend_id,
+                           avg_distance=distance_map[missed_friend_id],
+                           last_distance=distance_map[missed_friend_id],
+                           last_seen=int(time.time()),
+                           last_checkin=checkin_map[missed_friend_id],
+                           num_points=1)
+        friend.put() # Bulk action? or perhaps parallelized
+        logging.info('Created new record for ' + missed_friend_id)
+    else:
+      logging.info('ruh-roh')
+    
+    
 
 class FetchCheckins(webapp.RequestHandler):
   """Fetch the checkins we've received via push for the current user."""
@@ -81,14 +185,26 @@ class FetchCheckins(webapp.RequestHandler):
       ret = [c.checkin_json for c in checkins]
     self.response.out.write('['+ (','.join(ret)) +']')
 
+class FetchDistances(webapp.RequestHandler):
+  """Fetch the checkins we've received via push for the current user."""
+  def get(self):
+    user = UserToken.all().filter("user = ", users.get_current_user()).get()
+    ret = []
+    if user:
+      distances = FriendDistance.all().filter("fs_id = ", user.fs_id).fetch(1000)
+      ret = [json.dumps(d.to_dict()) for d in distances]
+    self.response.out.write('['+ (','.join(ret)) +']')
+
+
 class GetConfig(webapp.RequestHandler):
   """Returns the OAuth URI as JSON so the constants aren't in two places."""
   def get(self):
     uri = '%(server)s/oauth2/authenticate?client_id=%(client_id)s&response_type=code&redirect_uri=%(redirect_uri)s' % config
-    self.response.out.write(simplejson.dumps({'auth_uri': uri}))
+    self.response.out.write(json.dumps({'auth_uri': uri}))
 
 application = webapp.WSGIApplication([('/oauth', OAuth), 
                                       ('/checkin', ReceiveCheckin),
+                                      ('/distances', FetchDistances),
                                       ('/fetch', FetchCheckins),
                                       ('/config', GetConfig)],
                                      debug=True)
